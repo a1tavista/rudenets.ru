f4f047b68dcb1e13fd219be9f05541055be11341
Andrew Rudenets <andrey.rudenets@gmail.com>
2017-04-30 10:01:39 +0300

    Use default layouts for clearance

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -2,7 +2,3 @@ Clearance.configure do |config|
   config.allow_sign_up = false
   config.routes = true
 end
-
-Clearance::PasswordsController.layout "admin"
-Clearance::UsersController.layout "admin"
-Clearance::BaseController.layout "admin"
