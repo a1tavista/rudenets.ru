35d7d014e04a9d4bbb6f5f705bb0088213a6a870
Andrew Rudenets <andrey.rudenets@gmail.com>
2017-05-21 17:40:34 +0300

    Store files from development in separate folder

--- app/uploaders/amazon_uploader.rb
+++ app/uploaders/amazon_uploader.rb
@@ -2,6 +2,8 @@ class AmazonUploader < CarrierWave::Uploader::Base
   storage :fog
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    directory = "dev_uploads" if Rails.env.development?
+    directory ||= "uploads"
+    "#{directory}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 end
