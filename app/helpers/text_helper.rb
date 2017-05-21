1048deaf1cd5600ff1e373940ba0f44ce4dd739a
Andrew Rudenets <andrey.rudenets@gmail.com>
2017-05-21 17:41:18 +0300

    Fix issue with MD/SmartyPants

--- app/helpers/text_helper.rb
+++ app/helpers/text_helper.rb
@@ -1,11 +1,13 @@
 module TextHelper
   def to_markdown(text)
+    return nil if text.nil?
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, footnotes: true, tables: true)
     html = markdown.render(text)
     to_smarty(html)
   end
 
   def to_smarty(html)
+    return nil if html.nil?
     Redcarpet::Render::SmartyPants.render(html).html_safe
   end
 end
