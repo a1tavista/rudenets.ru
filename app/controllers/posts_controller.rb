c23eab0dab0fc0d01e5e81294cd97537345ee944
Andrew Rudenets <andrey.rudenets@gmail.com>
2017-06-04 19:23:02 +0300

    Add markdown compiling into PostForm component

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,7 @@
 class PostsController < ApplicationController
   load_and_authorize_resource
+
+  def edit
+    render layout: "editor"
+  end
 end
--- app/frontend/components/PostForm.vue
+++ app/frontend/components/PostForm.vue
@@ -1,7 +1,47 @@
 <script>
-  export default {}
+  import _ from 'lodash';
+  import marked from 'marked';
+
+  export default {
+    data() {
+      return {
+        input: "",
+        isPreview: false
+      }
+    },
+    computed: {
+      compiledMarkdown: function () {
+        if (this.input) {
+          return marked(this.input, {sanitize: true});
+        } else {
+          return "";
+        }
+      }
+    },
+    methods: {
+      update: _.debounce(function (e) {
+        this.input = e.target.value
+      }, 300)
+    },
+    mounted() {
+      marked.setOptions({
+        smartLists: true,
+        smartypants: true
+      });
+      this.input = this.mdText;
+    },
+    props: ["mdText"]
+  }
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <button @click="isPreview = !isPreview"></button>
+    <article class="post">
+      <div class="post-form">
+        <div class="post__content post-form__preview" v-html="compiledMarkdown" v-show="isPreview"></div>
+        <textarea class="post-form__editor" v-model="input" v-show="!isPreview"></textarea>
+      </div>
+    </article>
+  </div>
 </template>
