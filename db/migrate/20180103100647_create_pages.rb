30a87fc34074ea96e74783a3126514435047b8a5
Andrew Rudenets <andrey.rudenets@gmail.com>
2018-01-03 14:19:26 +0300

    Don't use null as id value

--- db/migrate/20180103100647_create_pages.rb
+++ db/migrate/20180103100647_create_pages.rb
@@ -5,6 +5,6 @@ class CreatePages < ActiveRecord::Migration[5.1]
       t.string :title
       t.string :content
     end
-    ActiveRecord::Base.connection.execute("INSERT INTO pages VALUES (null, 'about', 'Обо мне', 'nothing')")
+    ActiveRecord::Base.connection.execute("INSERT INTO pages VALUES (1, 'about', 'Обо мне', 'nothing')")
   end
 end
