6a885ffb416e646434660d151e0ebe38c6242a7f
Andrew Rudenets <andrey.rudenets@gmail.com>
2019-09-17 20:30:08 +0300

    Prettify form for signing in

--- app/controllers/backoffice_controller.rb
+++ app/controllers/backoffice_controller.rb
@@ -5,4 +5,8 @@ class BackofficeController < ApplicationController
   def backoffice
     render layout: :backoffice
   end
+
+  def url_after_denied_access_when_signed_out
+    cc_url
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180508090300) do
+ActiveRecord::Schema.define(version: 2018_05_08_090300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20180508090300) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "delayed_jobs", force: :cascade do |t|
+  create_table "delayed_jobs", id: :serial, force: :cascade do |t|
     t.integer "priority", default: 0, null: false
     t.integer "attempts", default: 0, null: false
     t.text "handler", null: false
