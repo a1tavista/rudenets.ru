38c1f9fddd02d8549d9b0cf272589cf4f3c6d95a
Andrew Rudenets <andrey.rudenets@gmail.com>
2020-01-25 22:36:01 +0300

    Fix optimizing images for posts content

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,21 +1,17 @@
+require "image_processing/mini_magick"
+
 class ImageUploader < Shrine
   plugin :derivatives
 
   Attacher.derivatives do |original|
     optimized = Optimizer.new(original).call
+    magick = ::ImageProcessing::MiniMagick.source(optimized)
 
     {
       optimized: optimized,
-      large: prepare_thumbnail(optimized, dimensions: [960, 960]),
-      medium: prepare_thumbnail(optimized, dimensions: [1024, 1024]),
-      small: prepare_thumbnail(optimized, dimensions: [2048, 2048]),
+      large: magick.resize_to_fit!(1000, 1000),
+      medium: magick.resize_to_fit!(600, 600),
+      small: magick.resize_to_fit!(300, 300),
     }
-  end
-
-  private
-
-  def prepare_thumbnail(image, dimensions:)
-    magick = ImageProcessing::MiniMagick.source(image)
-    magick.resize_to_fit!(*dimensions)
   end
 end
